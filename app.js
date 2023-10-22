430c11672d30369a7a0977aace37addbf07a3b05
Aleksandar Petrov <sasho1320@gmail.com>
2023-10-22 08:48:09 +0300

    Update app.js file

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        message: 'Hello from the server side!',
-        app: 'Natours',
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours,
+        },
     });
 });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...');
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    if (!tour) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour,
+        },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/api/v1/tours`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    });
 });
 
 const port = 3501;
